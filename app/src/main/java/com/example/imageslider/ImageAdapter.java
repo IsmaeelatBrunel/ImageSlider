7ff5ed1806d294dd55d08126e8652a0e2e810cb9
IsmaeelatBrunel <1708632@brunel.ac.uk>
2019-03-16 16:53:05 +0000

    Created app that allows image slider functionality to work. IK

---
+++ app/src/main/java/com/example/imageslider/ImageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.imageslider;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private int[] mImageIds = new int[] {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5};
+
+    ImageAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView,0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ImageView) object);
+    }
+
+}
